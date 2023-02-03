10ceba0d02e2f8dfae31cd0937e86ce39a7786e7
captnseagraves <k.s.seagraves@gmail.com>
2023-02-03 16:37:52 -0700

    storage packing

--- src/interfaces/sellerFinancing/ISellerFinancingStructs.sol
+++ src/interfaces/sellerFinancing/ISellerFinancingStructs.sol
@@ -3,27 +3,35 @@ pragma solidity 0.8.13;
 
 interface ISellerFinancingStructs {
     struct Offer {
-        // Offer creator
-        address creator;
+        // SLOT 0
         uint128 price;
         uint128 downPaymentAmount;
-        uint32 minimumPrincipalPerPeriod;
-        uint32 periodInterestRateBps;
-        uint32 periodDuration;
-        // offer NFT contract address
-        address nftContractAddress;
+        // SLOT 1 - 128 remaining
+        uint128 minimumPrincipalPerPeriod;
+        // SLOT 2
         // offer NFT ID
         uint256 nftId;
+        // SLOT 3 - 96 remaining
+        // offer NFT contract address
+        address nftContractAddress;
+        // Offer creator
+        address creator;
+        uint32 periodInterestRateBps;
+        uint32 periodDuration;
         uint32 expiration;
     }
 
     struct Loan {
+        // SLOT 0
         // The current borrower of a loan
         uint256 buyerNftId;
+        // SLOT 1
         // The current lender of a loan
         uint256 sellerNftId;
+        // SLOT 2
         uint128 remainingPrincipal;
-        uint32 minimumPrincipalPerPeriod;
+        uint128 minimumPrincipalPerPeriod;
+        // SLOT 3 - 128 remaining
         uint32 periodInterestRateBps;
         uint32 periodDuration;
         uint32 periodEndTimestamp;
