4561e6f57b1e5b73324c9bde74c6de5f2bd24641
captnseagraves <k.s.seagraves@gmail.com>
2023-02-09 15:48:24 -0700

    getOfferSigner test passing

---
+++ test/unit/getOfferSigner.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../common/BaseTest.sol";
+import "./../utils/fixtures/OffersLoansFixtures.sol";
+import "../../src/interfaces/sellerFinancing/ISellerFinancingStructs.sol";
+
+contract TestGetOfferSigner is
+    Test,
+    BaseTest,
+    ISellerFinancingStructs,
+    OffersLoansFixtures
+{
+    uint256 immutable SIGNER_PRIVATE_KEY_1 =
+        0x60b919c82f0b4791a5b7c6a7275970ace1748759ebdaa4076d7eeed9dbcff3c3;
+    address immutable SIGNER_1 = 0x503408564C50b43208529faEf9bdf9794c015d52;
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function test_unit_getOfferSigner() public {
+        Offer memory offer = Offer({
+            creator: seller1,
+            nftContractAddress: address(
+                0xB4FFCD625FefD541b77925c7A37A55f488bC69d9
+            ),
+            nftId: 1,
+            price: 1 ether,
+            downPaymentAmount: 0.3 ether,
+            minimumPrincipalPerPeriod: 0.07 ether,
+            periodInterestRateBps: 25,
+            periodDuration: 30 days,
+            expiration: uint32(1657217355)
+        });
+
+        bytes32 offerHash = sellerFinancing.getOfferHash(offer);
+
+        bytes memory signature = sign(SIGNER_PRIVATE_KEY_1, offerHash);
+
+        address signer = sellerFinancing.getOfferSigner(offer, signature);
+
+        assertEq(signer, SIGNER_1);
+    }
+}
