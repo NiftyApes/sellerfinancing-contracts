a5581a7ac9d15bc469c732155a8069885afef2ef
captnseagraves <k.s.seagraves@gmail.com>
2023-02-24 14:36:59 -0700

    add goerli implementatino deply script

---
+++ script/Goerli_Deploy_SellerFinancingImplementation.s.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+
+import "@openzeppelin-norm/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin-norm/contracts/proxy/transparent/ProxyAdmin.sol";
+
+import "../src/interfaces/sellerFinancing/ISellerFinancing.sol";
+import "../src/interfaces/Ownership.sol";
+
+import "../src/SellerFinancing.sol";
+
+contract DeploySellerFinancingScript is Script {
+    NiftyApesSellerFinancing sellerFinancing;
+
+    function run() external {
+        address goerliRoyaltiesEngineAddress = 0xe7c9Cb6D966f76f3B5142167088927Bf34966a1f;
+
+        vm.startBroadcast();
+
+        // deploy and initialize implementation contracts
+        sellerFinancing = new NiftyApesSellerFinancing();
+        sellerFinancing.initialize(goerliRoyaltiesEngineAddress);
+
+        // pauseSanctions for Goerli as Chainalysis contact doesnt exists there
+        sellerFinancing.pauseSanctions();
+
+        vm.stopBroadcast();
+    }
+}
