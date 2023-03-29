cf3c0d1c8aa5aef9aaf4ebe1fb9aee783cd20f92
captnseagraves <k.s.seagraves@gmail.com>
2023-03-29 15:27:10 -0600

    fix for loans on loans

--- src/interfaces/sellerFinancing/ISellerFinancingErrors.sol
+++ src/interfaces/sellerFinancing/ISellerFinancingErrors.sol
@@ -23,6 +23,8 @@ interface ISellerFinancingErrors {
 
     error LoanAlreadyClosed();
 
+    error CannotBuySellerFinancingTicket();
+
     error InvalidOffer0ItemType(ISeaport.ItemType given, ISeaport.ItemType expected);
 
     error InvalidOffer0Token(address given, address expected);
